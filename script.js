8819927f9a1755e58745ee79ce5974e125e59bdd
Aaditya785 <aadityasandil5@gmail.com>
2023-05-09 17:19:48 +0530

    DOM manupilation done

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 
-let level = 'hard';
-const url = `https://opentdb.com/api.php?amount=10&difficulty=${level}&type=boolean`;
+// const level = $("#level").text();
+// const category = $("#Category").text();
+const sub_btn = $("#opt_box_btn");
+const load = $("#loading");
+
+
+sub_btn.click(()=>{
+	const category_text = $('#Category option:selected').text();
+	$('#info_catg').text(category_text);
+	
+	const level = $('#level option:selected').val();
+	const category = $('#Category option:selected').val();
+	
+	const url = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${level}&type=boolean`;
+	here(url);
+	
+	$(".opt_box").hide();
+	$(".main").show();
+})
+
+
+
+
 
 async function here (url){
 	const response = await fetch(url);
 	const result = await response.json();
+
+	console.log(result)
+
 	const question = result.results[0];
 	const options = [...question.incorrect_answers, question.correct_answer].sort();
 	$("#ctgy").append(" "+question.category);
 	$("#qst").append(" "+question.question);
 	const div = $(".t_f");
   options.forEach(option => {
-    const btn = $("<button>");
+    const btn = $("<button id='ans'>");
     btn.text(option);
     div.append(btn);
   });
   $("div").append(div);
   
 };
-here(url);
+
+
